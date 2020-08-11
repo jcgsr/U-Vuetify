f95e56052cff5c1e48f59b03df6bbf3ba8f1e2ae
jcgsr <cabralguerra@gmail.com>
2020-08-11 19:49:56 -0300

    comprimento

---
+++ src/components/conversor/comprimentos/Comprimento.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="comprimento">
+     <v-card class="mx-auto mb-3 mt-3 animate__animated animate__fadeInLeft" max-width="544" outlined>
+      <v-card-title>Pé para Metro</v-card-title>
+      <v-input>
+        <v-text-field @focus="$event.target.select()" type="number" name="number" v-model="number"></v-text-field>
+      </v-input>
+      <h3 class="display-3 mb-5">{{ result }}</h3>
+    </v-card>
+
+    <v-card class="mx-auto mb-3 mt-3 animate__animated animate__fadeInLeft" max-width="544" outlined>
+      <v-card-title>Metro para Pé</v-card-title>
+      <v-input>
+        <v-text-field @focus="$event.target.select()" type="number" name="number1" v-model="number1"></v-text-field>
+      </v-input>
+      <h3 class="display-3 mb-5">{{ result1 }}</h3>
+    </v-card>
+    <MilhasKm />
+    <PolegadaCm />
+  </div>  
+</template>
+
+<script>
+import MilhasKm from './MilhasKm'
+import PolegadaCm from './PolegadaCm'
+export default {
+  data() {
+    return {
+      subtitulo: "Pés - Metro",
+      subtitulo1: "Metro - Pés",
+      number: 1,
+      number1: 1,
+    };
+  },
+
+  computed: {
+    result() {
+      return (this.number / 3.281).toFixed(2) + 'ft' 
+    },
+    result1() {
+      return (this.number1 * 3.281).toFixed(2) + 'm'
+    }
+  },
+  components: {
+    MilhasKm, PolegadaCm
+  }
+};
+</script>
+
+<style>
+.comprimento {
+  color: aliceblue;
+  text-align: center;
+}
+
+h3 {
+  padding: 1rem;
+}
+</style>
--- src/components/conversor/comprimentos/PolegadaCm.vue
+++ src/components/conversor/comprimentos/PolegadaCm.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="massa">
+  <div class="polegada">
     <v-card class="mx-auto mb-3 mt-3 animate__animated animate__fadeInLeft" max-width="544" outlined>
-      <v-card-title>Libra para Kilograma</v-card-title>
+      <v-card-title>Polegada para Centímetro</v-card-title>
       <v-input>
         <v-text-field @focus="$event.target.select()" type="number" name="number" v-model="number"></v-text-field>
       </v-input>
       <h3 class="display-3 mb-5">{{ result }}</h3>
     </v-card>
 
-   <v-card class="mx-auto mb-3 mt-3 animate__animated animate__fadeInRight" max-width="544" outlined>
-      <v-card-title>Kilograma para Libra</v-card-title>
+    <v-card class="mx-auto mb-3 mt-3 animate__animated animate__fadeInLeft" max-width="544" outlined>
+      <v-card-title>Centímetro para Polegada</v-card-title>
       <v-input>
         <v-text-field @focus="$event.target.select()" type="number" name="number1" v-model="number1"></v-text-field>
       </v-input>
@@ -21,7 +21,7 @@
 <script>
 export default {
   data() {
-    return {      
+    return {
       number: 1,
       number1: 1,
     };
@@ -29,17 +29,17 @@ export default {
 
   computed: {
     result() {
-      return (this.number / 2.205).toFixed(2) + 'kg' 
+      return (this.number * 2.54).toFixed(2) + 'cm' 
     },
     result1() {
-      return (this.number1 * 2.205).toFixed(2) + 'lb'
+      return (this.number1 / 2.54).toFixed(2) + 'in'
     }
   }
 };
 </script>
 
 <style>
-
+.
 h3 {
   padding: 1rem;
 }
