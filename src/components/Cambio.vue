733462f85eed5ab05ac3631bbecca73a83f5b1af
jcgsr <cabralguerra@gmail.com>
2020-11-08 00:52:08 -0300

    cambio refatorado

--- src/components/Cambio.vue
+++ src/components/Cambio.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="cambio">
     <h2 class="display-3 mt-3 mb-3">{{ title }}</h2>
+    <div class="button-div">
+      <v-btn secondary style="color:rgb(235, 235, 21)"
+      @click="dollarreal"        
+      class="animate__animated animate__backInLeft"
+      >US$ - R$</v-btn>
+       
+       <v-btn secondary style="color:rgb(235, 235, 21)"
+      @click="euroreal"        
+      class="animate__animated animate__backInLeft"
+      >€ - R$</v-btn>
+
+       <v-btn secondary style="color:rgb(235, 235, 21)"
+      @click="poundreal"        
+      class="animate__animated animate__backInLeft"
+      >£ - R$</v-btn>
+
+    </div>
     <div class="conversor">
-      <ConversorDollarReal moeda-a="USD" moeda-b="BRL" />
-      <ConversorEuroReal moeda-a="EUR" moeda-b="BRL" />
-      <ConversorLibraReal moeda-a="GBP" moeda-b="BRL" />
-      <ConversorRealDollar moeda-a="BRL" moeda-b="USD" />
-      <ConversorRealEuro moeda-a="BRL" moeda-b="EUR" />
-      <ConversorRealLibra moeda-a="BRL" moeda-b="GBP" />
+      <ConversorDollarReal v-if="showDollarReal" moeda-a="USD" moeda-b="BRL" />
+      <ConversorEuroReal v-if="showEuroReal" moeda-a="EUR" moeda-b="BRL" />
+      <ConversorLibraReal v-if="showPoundReal" moeda-a="GBP" moeda-b="BRL" />
+      <ConversorRealDollar v-if="showDollarReal" moeda-a="BRL" moeda-b="USD" />
+      <ConversorRealEuro v-if="showEuroReal" moeda-a="BRL" moeda-b="EUR" />
+      <ConversorRealLibra v-if="showPoundReal" moeda-a="BRL" moeda-b="GBP" />
     </div>
   </div>
 </template>
@@ -25,11 +42,44 @@ export default {
   name: "Cambio",
   data: () => ({
     title: "Taxa de Câmbio",
+    showDollarReal: true,
+    showEuroReal: false,
+    showPoundReal: false,
   }),
+  methods: {
+    dollarreal() {
+      this.showDollarReal = true
+      this.showEuroReal = false
+      this.showPoundReal = false
+    },
+    euroreal() {
+      this.showDollarReal = false
+      this.showEuroReal = true
+      this.showPoundReal = false
+    },
+    poundreal() {
+      this.showDollarReal = false
+      this.showEuroReal = false
+      this.showPoundReal = true
+    }
+  }
 };
 </script>
 
 <style lang="css">
+.button-div {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  text-align: center;
+  max-width: 15%;
+}
+
+.button-div button {
+  margin-left: .5rem;
+  color: rgba(245, 245, 7, 0.479);
+}
 @media only screen and (max-width: 456px) {
   #cambio {
     width: 85%;
